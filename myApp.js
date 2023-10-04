84b9d175d0b94c4886f905a2885bd94c1e886414
PetrosaMan <dw.todd.eng@gmail.com>
2023-10-04 22:04:33 +0200

    relocated use function near top of code

--- myApp.js
+++ myApp.js
@@ -2,43 +2,42 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let app = express();
 
+// #1
+console.log("Hello World", express.static);
+
+// #2
+app.get('/', (req, res) => {
+  res.send("Hello Express");
+});
+
 // parse urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// # 11 parse application/json
+// #11 parse application/json
 app.use(bodyParser.json);
-/*
+
+//#4
+app.use("/public", express.static( __dirname + "/public"));
+
 // #7
 app.use('/',function simpleLogger(req, res, next) {
     console.log(req.method + " " + req.path + " - " + req.ip); 
   next();
 });
-*/
 
-// #1
-//console.log("Hello World", express.static);
-
-// #2
-//app.get('/', (req, res) => {
-//  res.send("Hello Express");
-//});
-
-// #4
-//app.use("/public", express.static( __dirname + "/public"));
+// #8
+app.use('/now', express.static(__dirname + '/now'));
 
 // #3
-//app.get("/", function(req, res) {
-//  res.sendFile(__dirname + //"/views/index.html");
-//});
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
-/*
 // #5
 app.get('/json', (req, res) => {
   res.json({"message": "Hello json"});
 });
-*/
 
-/*
 // #6
 app.get('/json', (req, res) => {
   if (process.env["MESSAGE_STYLE"] === uppercase) {
@@ -47,11 +46,6 @@ app.get('/json', (req, res) => {
       res.json({"message": "Hello json"});
   }
 });
-*/
-
-// #8
-/*
-app.use('/now', express.static(__dirname + '/now'));
 
 app.get("/now",(req, res, next) => {
     req.time = new Date().toString();
@@ -62,15 +56,12 @@ app.get("/now",(req, res, next) => {
     res.json({"time": req.time});
   }
 );
-*/
 
-/*
 // #9 
-app.get('/:word/echo', (req, res)=>{     
+app.get('/word/echo', (req, res)=>{     
   res.json({echo: req.params.word});
 });
-*/
-/*
+
 // #10
 app.get('/name', function (req, res) {
 	const first = req.query.first;
@@ -78,13 +69,10 @@ app.get('/name', function (req, res) {
         res.json({ "name": first + ' ' + last });
     });
 
-*/
-
 // # 12
 app.post('/name', function(req, res){
   // handle the data in the object req.body
-  let Name = req.body.first + " " + req.body.last;
-    
+  let Name = req.body.first + " " + req.body.last;    
   res.json({ "name": Name });
 });
 
