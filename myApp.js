c0e19d0a7e8667053828d97f33aaa7d5ec0e1037
PetrosaMan <dw.todd.eng@gmail.com>
2023-10-06 10:21:34 +0200

    restructured and tested code

--- myApp.js
+++ myApp.js
@@ -1,23 +1,22 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let app = express();
+require('dotenv').config();
 
 // #1
-console.log("Hello World", express.static);
+//console.log("Hello World");
 
 // #2
-app.get('/', (req, res) => {
-  res.send("Hello Express");
-});
+//app.get('/', (req, res) => {
+//  res.send("Hello Express");
+//});
 
+// #11
 // parse urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// #11 parse application/json
-app.use(bodyParser.json);
-
 //#4
-app.use("/public", express.static( __dirname + "/public"));
+app.use('/public', express.static( __dirname + '/public'));
 
 // #7
 app.use('/',function simpleLogger(req, res, next) {
@@ -28,27 +27,27 @@ app.use('/',function simpleLogger(req, res, next) {
 // #8
 app.use('/now', express.static(__dirname + '/now'));
 
+// #11 parse application/json
+app.use(bodyParser.json);
+
 // #3
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+app.get('/', function(req, res) {
+ res.sendFile(__dirname + "/views/index.html");
 });
 
-// #5
+// #5 Serve JSON on a Specific Route
 app.get('/json', (req, res) => {
   res.json({"message": "Hello json"});
 });
 
-// #6
+// #6 Use the .env File
 app.get('/json', (req, res) => {
-  if (process.env["MESSAGE_STYLE"] === uppercase) {
-      res.json({"message": "HELLO JSON"});        
-  } else {
-      res.json({"message": "Hello json"});
-  }
+   res.json({"message": process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json"});
 });
 
+// # 8
 app.get("/now",(req, res, next) => {
-    req.time = new Date().toString();
+   req.time = new Date().toString();
     console.log(req.time);
     next();
   },
@@ -58,22 +57,22 @@ app.get("/now",(req, res, next) => {
 );
 
 // #9 
-app.get('/word/echo', (req, res)=>{     
-  res.json({echo: req.params.word});
+app.get('/:word/echo', (req, res)=>{ 
+  let word = req.params.word;    
+  res.json({echo: word});
 });
 
-// #10
+//#10
 app.get('/name', function (req, res) {
 	const first = req.query.first;
 	const last = req.query.last;
-        res.json({ "name": first + ' ' + last });
-    });
+        res.json({ "name": first + " " + last });
+});
 
 // # 12
 app.post('/name', function(req, res){
   // handle the data in the object req.body
-  let Name = req.body.first + " " + req.body.last;    
-  res.json({ "name": Name });
+  res.json({ "name": req.body.first + " " + req.body.last });
 });
 
 
