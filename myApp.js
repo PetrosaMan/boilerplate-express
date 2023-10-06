428922e54a7838853af22b09f738f460aa0b1fe6
PetrosaMan <dw.todd.eng@gmail.com>
2023-10-06 23:27:12 +0200

    made changes to code layout and fixed errors

--- myApp.js
+++ myApp.js
@@ -4,49 +4,50 @@ let bodyParser = require('body-parser');
 
 require('dotenv').config();
 
-// #1
+// #1 meet the console
 console.log("Hello World");
 
-// #2
-app.get('/', (req, res) => {
-  res.send("Hello Express");
-});
+// #2 start a working express server
+//app.get('/', (req, res) => {
+  //res.send("Hello Express");
+//});
 
-// #11
-// parse urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//#4
-app.use('/public', express.static( __dirname + '/public'));
-
-// #7
+// #7 mount the logger middleware
 app.use('/',function simpleLogger(req, res, next) {
-    console.log(req.method + " " + req.path + " - " + req.ip); 
-  next();
+  console.log(req.method + " " + req.path + " - " + req.ip); 
+next();
 });
 
-// #8
+// #11 mount the body parser middleware
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+
+// #3 serve an html file
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+//#4 serve static assets
+app.use('/', express.static( __dirname + '/public'));
+
+
 app.use('/now', express.static(__dirname + '/now'));
 
-// #11 parse application/json
-app.use(bodyParser.json());
-
-// #3
-app.get('/', function(req, res) {
-res.sendFile(__dirname + "/views/index.html");
-});
-
 // #5 Serve JSON on a Specific Route
-app.get('/json', (req, res) => {
-  res.json({"message": "Hello json"});
-});
+//app.get('/json', (req, res) => {
+  //res.json({"message": "Hello json"});
+//});
 
 // #6 Use the .env File
 app.get('/json', (req, res) => {
    res.json({"message": process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json"});
 });
 
-// # 8
+
+// # 8  chain middleware to create a time server
 app.get("/now",(req, res, next) => {
    req.time = new Date().toString();
     console.log(req.time);
@@ -57,25 +58,21 @@ app.get("/now",(req, res, next) => {
   }
 );
 
-// #9 
+// #9  get route parameter input from the client
 app.get('/:word/echo', (req, res)=>{ 
   let word = req.params.word;    
   res.json({echo: word});
 });
 
-//#10
-app.get('/name', function (req, res) {
-	const first = req.query.first;
-    const last = req.query.last;
-        res.json({ "name": first + " " + last });
-});
+//#10 get query parameter input from the client
+//app.get('/name', function (req, res) {	
+//    res.json({ "name": req.query.first + " " + req.query.last });
+//});
 
-// # 12
+// # 12 get data from post requests
 app.post('/name', function(req, res){
-  // handle the data in the object req.body
-  //res.json({ name: req.body.first + " " + req.body.last });
-  let string = req.body.first + " " + req.body.last;
-  res.json({name: string});
+  // handle the data in the object req.body  
+   res.json({name: req.body.first + " " + req.body.last});
 });
 
 
