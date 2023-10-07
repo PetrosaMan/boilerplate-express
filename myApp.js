a499c9a2508e3d8f72eef8c7a2dea693bcf4161c
PetrosaMan <dw.todd.eng@gmail.com>
2023-10-07 11:14:39 +0200

    changed order of code parts

--- myApp.js
+++ myApp.js
@@ -5,24 +5,25 @@ let bodyParser = require('body-parser');
 require('dotenv').config();
 
 // #1 meet the console
-console.log("Hello World");
+//console.log("Hello World");
 
 // #2 start a working express server
 //app.get('/', (req, res) => {
   //res.send("Hello Express");
 //});
 
-// #7 mount the logger middleware
-app.use('/',function simpleLogger(req, res, next) {
+//#4 mount serve static assets
+app.use('/public', express.static( __dirname + '/public'));
+
+// #7 mount the logger middleware??? Check this
+app.use(function simpleLogger(req, res, next) {
   console.log(req.method + " " + req.path + " - " + req.ip); 
 next();
 });
 
 // #11 mount the body parser middleware
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
+//app.use( bodyParser.json() );  is this required ????????    
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 // #3 serve an html file
@@ -30,22 +31,18 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-//#4 serve static assets
-app.use('/', express.static( __dirname + '/public'));
-
-
+// not sure where this should be located??
 app.use('/now', express.static(__dirname + '/now'));
 
 // #5 Serve JSON on a Specific Route
 //app.get('/json', (req, res) => {
-  //res.json({"message": "Hello json"});
+//  res.json({"message": "Hello json"});
 //});
 
 // #6 Use the .env File
 app.get('/json', (req, res) => {
    res.json({"message": process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json"});
 });
-
 
 // # 8  chain middleware to create a time server
 app.get("/now",(req, res, next) => {
@@ -59,15 +56,14 @@ app.get("/now",(req, res, next) => {
 );
 
 // #9  get route parameter input from the client
-app.get('/:word/echo', (req, res)=>{ 
-  let word = req.params.word;    
-  res.json({echo: word});
-});
+//app.get('/:word/echo', (req, res)=>{      
+//  res.json({word: req.params.word});  
+//});
 
 //#10 get query parameter input from the client
-//app.get('/name', function (req, res) {	
-//    res.json({ "name": req.query.first + " " + req.query.last });
-//});
+app.get('/name', function (req, res) {	
+    res.json({ name: req.query.first + " " + req.query.last });
+});
 
 // # 12 get data from post requests
 app.post('/name', function(req, res){
